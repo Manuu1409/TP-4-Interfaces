ec09c546c0ec937731dd7ca4fd836dafa9488eef
Manuu1409 <manuracingclub61@gmail.com>
2023-11-17 20:48:34 -0300

    menu hamburguesa, animaciones

---
+++ animaciones.js
@@ -0,0 +1,12 @@
+let estado = false; 
+
+let menuHamburguesa = document.getElementById('menuHamburguesa');
+
+menuHamburguesa.addEventListener('click', function() {
+  if (estado) {
+    menuHamburguesa.src = 'images/menu-hamburguesa.png';
+  } else {
+    menuHamburguesa.src = 'images/cruz.png'; 
+  }
+  estado = !estado; 
+});
