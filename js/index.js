a256ad673b917a770f323ddfcc7a876a3e1ff498
Nicolas Urdangarin <nicourdu@gmail.com>
2023-11-18 20:04:48 -0300

    arreglando problema header

--- js/index.js
+++ js/index.js
@@ -61,3 +61,27 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
+  let counter = document.querySelector('.counter');
+
+  function startCounter() {
+    let count = 0;
+  
+    const interval = setInterval(() => {
+      count++;
+      counter.textContent = count;
+  
+      if (count === 100) {
+        clearInterval(interval);
+        redirectToNewPage();
+      }
+    }, 80);
+  }
+  
+  function redirectToNewPage() {
+    setTimeout(function() {
+      window.location.href = '../html/index.html';
+    }, 1000); 
+  }
+  
+  window.onload = startCounter;
+  
